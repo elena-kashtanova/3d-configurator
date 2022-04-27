08af994b25a2d2caa970a6bf930767e4e64035c2
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-04-27 18:35:48 +0300

    Update model entity

--- src/entities/model.entity.ts
+++ src/entities/model.entity.ts
@@ -2,12 +2,17 @@ import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 class ModelEntity extends BaseEntity {
-    constructor(id: number, name: string, color: string, positions: number[]) {
+    constructor(id: number, name: string, type: string, color: string, position: number[], uv: number[], normal: number[], index: number[], parameters: JSON) {
         super();
         this.id = id;
         this.name = name;
+        this.type = type;
         this.color = color;
-        this.positions = positions;
+        this.position = position;
+        this.uv = uv;
+        this.normal = normal;
+        this.index = index;
+        this.parameters = parameters;
     }
 
     @PrimaryGeneratedColumn()
@@ -17,12 +22,36 @@ class ModelEntity extends BaseEntity {
     public name: string;
 
     @Column()
+    public type: string;
+
+    @Column()
     public color: string;
 
     @Column('float', {
         array: true,
     })
-    public positions: number[];
+    public position: number[];
+
+    @Column('float', {
+        array: true,
+    })
+    public uv: number[];
+
+    @Column('float', {
+        array: true,
+    })
+    public normal: number[];
+
+    @Column('integer', {
+        array: true,
+        nullable: true,
+    })
+    public index: number[] | null;
+
+    @Column('json', {
+        nullable: true,
+    })
+    public parameters: object;
 }
 
 export { ModelEntity };
