797f6ed8520bec9ff83c7ae223d43f7738d1b7b8
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-04-27 16:28:00 +0300

    Create model typeorm repo

---
+++ src/repositories/model.repository.ts
@@ -0,0 +1,29 @@
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+import ModelEntity from '../entities/model.entity';
+import IModelRepo from '../interfaces/model-repo.interface';
+
+class ModelTypeORMRepository implements IModelRepo<ModelEntity, UpdateResult, DeleteResult> {
+    private repo: Repository<ModelEntity>;
+
+    constructor(repo: Repository<ModelEntity>) {
+        this.repo = repo;
+    }
+
+    public async getAll(): Promise<ModelEntity[]> {
+        return await this.repo.createQueryBuilder('model').select().getMany();
+    }
+
+    public async getById(id: number): Promise<ModelEntity | null> {
+        return await this.repo.findOneBy({ id: id });
+    }
+
+    public async update(id: number, data: Partial<ModelEntity>): Promise<UpdateResult> {
+        return await this.repo.update(id, data);
+    }
+
+    public async delete(id: number): Promise<DeleteResult> {
+        return await this.repo.delete(id);
+    }
+}
+
+export default ModelTypeORMRepository;
