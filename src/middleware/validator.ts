760b861244d3901e99235bdf79e77528142060be
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-04-27 16:58:06 +0300

    Fix validator

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function isIdProvided(req: Request, res: Response, next: NextFunction) {
-    if (req.query.id) {
+    if (req.params.id) {
         next();
     } else {
         const err = new Error('Invalid ID');
