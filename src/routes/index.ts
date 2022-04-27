1a4bf30d4b433d683811122c6bf0794e536dbff0
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-04-27 16:51:50 +0300

    Fix status code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import modelRouter from './model.router';
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.status(200).send('Home');
+    res.status(StatusCodes.OK).send('Home');
 });
 router.use('/models', modelRouter);
 
