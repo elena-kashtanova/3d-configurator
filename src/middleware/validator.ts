033ca49a700c73c5c82f051ff1a32ae841382a71
Elena Kashtanova <e.kashtanova@andersenlab.com>
2022-04-27 17:05:37 +0300

    Edit validation middleware

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
-export function isIdProvided(req: Request, res: Response, next: NextFunction) {
+export function checkParamsId(req: Request, res: Response, next: NextFunction) {
     if (req.params.id) {
         next();
     } else {
-        const err = new Error('Invalid ID');
+        const err = new Error('ID is not provided');
         next(err);
     }
 }
--- src/routes/model.router.ts
+++ src/routes/model.router.ts
@@ -4,7 +4,7 @@ import ModelEntity from '../entities/model.entity';
 import ModelTypeORMRepository from '../repositories/model.repository';
 import ModelService from '../services/model.service';
 import ModelController from '../controllers/model.controller';
-import { isIdProvided } from '../middleware/validator';
+import { checkParamsId } from '../middleware/validator';
 
 const modelRepo = new ModelTypeORMRepository(AppDataSource.getRepository(ModelEntity));
 const modelService = new ModelService(modelRepo);
@@ -13,8 +13,8 @@ const modelController = new ModelController(modelService);
 const modelRouter = Router();
 
 modelRouter.get('/', modelController.getAllModels);
-modelRouter.get('/:id', isIdProvided, modelController.getModelById);
-modelRouter.put('/:id', isIdProvided, modelController.updateModel);
-modelRouter.delete('/:id', isIdProvided, modelController.deleteModel);
+modelRouter.get('/:id', checkParamsId, modelController.getModelById);
+modelRouter.put('/:id', checkParamsId, modelController.updateModel);
+modelRouter.delete('/:id', checkParamsId, modelController.deleteModel);
 
 export default modelRouter;
